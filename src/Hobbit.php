f6aa9bfe6013d12fb9ccdc63aef2b1909f13310d
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-11-04 18:01:47 -0700

    Add hobbit class plus tests

--- src/Hobbit.php
+++ src/Hobbit.php
@@ -1,10 +1,29 @@
 <?php
-class Direwolf
+class Hobbit
 {
-    private $name;
+    public $name;
+    public $disposition = 'homebody';
+    public $age;
+    public $isShort = true;
 
-    public function __construct(string $name)
+    public function __construct(string $name=null, int $age=0)
     {
         $this->name = $name;
+        $this->age = $age;
+    }
+
+    public function celebrateBirthday():void
+    {
+        $this->age += 1;
+    }
+
+    public function adult():bool
+    {
+        return $this->age > 32;
+    }
+
+    public function isOld():bool
+    {
+        return $this->age > 100;
     }
 }
